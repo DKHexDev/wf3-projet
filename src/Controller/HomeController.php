603106d47024ebe9d306090ddec7f5a10cdb19df
aline.at@outlook.fr <aline.at@outlook.fr>
2021-09-27 17:36:52 +0200

    Update header

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\RecipeRepository;
-use App\Filter\today\Today;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,17 +14,6 @@ class HomeController extends AbstractController
      */
     public function index(RecipeRepository $repository): Response
     {
-
-        // On définit ici les valeurs event et season en faisant appel aux fonctions static de la classe Today()
-        $event      = Today::closeToSpecial();
-        $season         = Today::getSeason();
-
-        $lastRecipes    = $repository->findBy(['description' => !'null'], ['createdAt' => 'DESC'], 3);
-        $seasonRecipes  = $repository->findBy(['season' => $season], null, 3);
-        $eventRecipes  = $repository->findBy(['event' => $event], null, 3);
-
-        dump($event);
-        dump($season);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
