d9b4d65537030e61d00b3790d1cc866b8ed77146
D.K <70153500+DKHexDev@users.noreply.github.com>
2021-09-27 16:52:16 +0200

    Add Recipe : View, List, Delete and add link Font Awesome

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RecipeController extends AbstractController
 {
@@ -14,15 +18,88 @@ class RecipeController extends AbstractController
      */
     public function index(RecipeRepository $repository): Response
     {
-        // Instanciation de la classe filtre ( à modifier ) qui renvoie un tableau de filtres
-        // l'objet filter sera nourri grâce à des requêtes ajax.
-
-
+        // Récupére toutes les recettes.
         $recipes = $repository->findAll();
 
-
+        // Retourne la vue.
         return $this->render('recipe/index.html.twig', [
             'controller_name' => 'RecipeController',
+            'recipes' => $recipes,
         ]);
     }
+
+    /**
+     * @Route("/recipe/create", name="recipe_create")
+     */
+    public function create(Request $request, SluggerInterface $slugger)
+    {
+        // Autorisation pour aller sur la page.
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $recipe = new Recipe();
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            // Génére le slug de la recette et l'applique.
+            $slug = $slugger->slug($recipe->getName())->lower();
+            $recipe->setSlug($slug);
+
+            // Définition de la date de création.
+            $recipe->setCreatedAt(new \DateTimeImmutable());
+
+            // TEMPORAIRE
+            $recipe->setIngredients([]);
+        
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($recipe);
+            $manager->flush();
+
+            // Message de succès pour informer l'utilisateur
+            $this->addFlash('green', 'La recette a bien été créé.');
+
+            // Redirection vers la nouvelle recette.
+            return $this->redirectToRoute('recipe_show', ['slug' => $recipe->getSlug()]);
+        }
+
+        // Retourne la vue.
+        return $this->render('recipe/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/recipe/{id}/delete", name="recipe_delete")
+     */
+    public function delete(Recipe $recipe)
+    {
+        // Autorisation pour aller sur la page.
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($recipe);
+        $manager->flush();
+        
+        // Message de succès pour informer l'utilisateur
+        $this->addFlash('red', "La recette a bien été supprimée.");
+
+        return $this->redirectToRoute('recipe_list');
+    }
+
+    /**
+     * @Route("/recipe/{slug}", name="recipe_show")
+     */
+    public function show(Recipe $recipe)
+    {
+        // Retourne la vue.
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+
+
 }
