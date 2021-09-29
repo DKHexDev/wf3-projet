c6935fb70ea0c18d52fbfdeca50a9c2af3325ede
Thomas <70153500+DKHexDev@users.noreply.github.com>
2021-09-29 12:18:27 +0200

    Update RecipeController

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -18,12 +18,88 @@ class RecipeController extends AbstractController
      */
     public function index(RecipeRepository $repository): Response
     {
+        // Récupére toutes les recettes.
+        $recipes = $repository->findLatest();
 
-            $recipes = $repository->findAll();
+        dump($recipes);
 
+        // Retourne la vue.
         return $this->render('recipe/index.html.twig', [
-            'recipe' => $recipes,
             'controller_name' => 'RecipeController',
+            'recipes' => $recipes,
+        ]);
+    }
+
+    /**
+     * @Route("/recipe/create", name="recipe_create")
+     */
+    public function create(Request $request, SluggerInterface $slugger)
+    {
+        // Autorisation pour aller sur la page.
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $recipe = new Recipe();
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            // Génére le slug de la recette et l'applique.
+            $slug = $slugger->slug($recipe->getName())->lower();
+            $recipe->setSlug($slug);
+
+            // Définition de la date de création.
+            $recipe->setCreatedAt(new \DateTimeImmutable());
+        
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($recipe);
+            $manager->flush();
+
+            // Message de succès pour informer l'utilisateur
+            $this->addFlash('green', 'La recette a bien été créé.');
+
+            // Redirection vers la nouvelle recette.
+            return $this->redirectToRoute('recipe_show', ['slug' => $recipe->getSlug()]);
+        }
+
+        // Retourne la vue.
+        return $this->render('recipe/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/recipe/{id}/edit", name="recipe_edit")
+     */
+    public function edit(Recipe $recipe, Request $request)
+    {
+        // Autorisation pour aller sur la page.
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // Si la recette n'est pas trouvée, on redirige vers la 404.
+        if (!$recipe) {
+            throw $this->createNotFoundException('Cette recette n\'existe pas');
+        }
+
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        // Vérification si le formulaire est soumis et valide
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('green', 'La recette a bien été modifié');
+            return $this->redirectToRoute('recipe_show', ['slug' => $recipe->getSlug()]);
+        }
+
+        // Retourne la vue.
+        return $this->render('recipe/edit.html.twig', [
+            'form' => $form->createView(),
+            'recipe' => $recipe,
         ]);
     }
 
