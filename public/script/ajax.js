9fd679f751e04be4ea96c822ace9e978e956e203
UlrickEvrard <88329654+UlrickEvrard@users.noreply.github.com>
2021-09-29 11:12:35 +0200

    Ajax

--- public/script/ajax.js
+++ public/script/ajax.js
@@ -1,22 +1,23 @@
 // Filtrage des produits
 
 // Remplacer la classe par la classe Tailwind adaptée
-let filtreCase = document.querySelector('.form-check-input');
-
+const filtreCase = document.querySelector('.form-check-input');
+let request = new XMLHttpRequest();
 
 filtreCase.addEventListener('click', (event) => {
 
     // #filtres est l'id à placer sur le formalaire des filtres.
     // On récupère ici toutes les données du formulaires (les filtres à renvoyer au fichier php )
-    const result = document.querySelector('#filtres');
+    let result = document.querySelector('#filtres');
     result = new FormData(result);
+    console.log(result);
 
     // On lance la requête ajax :
 
-    let request = new XMLHttpRequest();
-    request.open('GET', '/api/recipes', true);
+    request.open('POST', '/api/recipes', true);
 
-    // on envoie les données du form à la requête
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    
     request.send(result);
 
-});
+})
