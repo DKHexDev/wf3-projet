273c0d3cfe9e87ed9a53603f53477d072b8e9c40
D.K <70153500+DKHexDev@users.noreply.github.com>
2021-10-04 15:39:28 +0200

    Update Favorites System

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -1,54 +1,63 @@
 // Ajout ou suppression des recettes en favoris.
 
-$('.far').click(function (){
+$('.fav').click(function (){
 
     let StarFav = $(this);
     let FlashMessage = $('#FlashMessage');
 
-    $.get('http://localhost:8000/account/favorites/' + StarFav.attr('data-recipe') + '/add')
-    .then(function (response){
+    if(StarFav.children().hasClass("far"))
+    {
+        $.get('http://localhost:8000/account/favorites/' + StarFav.children().attr('data-recipe') + '/add')
+        .then(function (response){
 
-        FlashMessage.removeClass("container");
-        FlashMessage.html(`
-            <div class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 fixed w-full" role="alert">
-                <p class="font-bold">Information</p>
-                <p>${response}</p>
-            </div>
-        `);
-        StarFav.removeClass("far");
-        StarFav.addClass("fas");
+            FlashMessage.removeClass("container");
+            FlashMessage.html(`
+                <div class="bg-${response.color}-100 border-l-4 border-${response.color}-500 text-${response.color}-700 p-4 fixed w-full" role="alert">
+                    <p class="font-bold">Information</p>
+                    <p>${response.message}</p>
+                </div>
+            `);
 
-        setTimeout(() => {
-            FlashMessage.addClass("container");
-            FlashMessage.html(``);
-        }, 5000)
-        
-    });
-});
+            if (response.changeClass)
+            {
+                StarFav.children().removeClass("far");
+                StarFav.children().addClass("fas");
+            }
 
-$('.fas').click(function (){
+            setTimeout(() => {
+                FlashMessage.addClass("container");
+                FlashMessage.html(``);
+            }, 5000)
+            
+        });
+    };
 
-    let StarFav = $(this);
-    let FlashMessage = $('#FlashMessage');
+    if (StarFav.children().hasClass("fas"))
+    {
+        $.get('http://localhost:8000/account/favorites/' + StarFav.children().attr('data-recipe') + '/remove')
+        .then(function (response){
+    
+            FlashMessage.removeClass("container");
+            FlashMessage.html(`
+                <div class="bg-${response.color}-100 border-l-4 border-${response.color}-500 text-${response.color}-700 p-4 fixed w-full" role="alert">
+                    <p class="font-bold">Information</p>
+                    <p>${response.message}</p>
+                </div>
+            `);
 
-    $.get('http://localhost:8000/account/favorites/' + StarFav.attr('data-recipe') + '/remove')
-    .then(function (response){
-
-        FlashMessage.removeClass("container");
-        FlashMessage.html(`
-            <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 fixed w-full" role="alert">
-                <p class="font-bold">Information</p>
-                <p>${response}</p>
-            </div>
-        `);
-        StarFav.removeClass("fas");
-        StarFav.addClass("far");
-
-        setTimeout(() => {
-            FlashMessage.addClass("container");
-            FlashMessage.html(``);
-        }, 5000)
-        
-    });
+            if(response.changeClass)
+            {
+                StarFav.children().removeClass("fas");
+                StarFav.children().addClass("far");
+            }
+    
+            setTimeout(() => {
+                FlashMessage.addClass("container");
+                FlashMessage.html(``);
+            }, 5000)
+            
+        });
+    };
+    
 });
 
--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -35,13 +35,11 @@ class AccountController extends AbstractController
         $user = $this->getUser();
 
         // Si la recette n'est pas trouvée, on redirige vers la 404.
-        if (!$recipe) {
-            throw $this->createNotFoundException('Cette recette n\'existe pas');
-        }
+        if (!$recipe) return $this->json(["color" => "red", "message" => "Cette recette n'existe pas.", "changeClass" => false]);
 
         // Si l'utilisateur n'est pas connecté, on le renvoie sur
         // la page de connexion.
-        if (!$user) return $this->redirectToRoute('app_login');
+        if (!$user) return $this->json(["color" => "red", "message" => "Impossible, vous devez avoir un compte pour ajouter une recette à vos favoris.", "changeClass" => false]);
 
         // Ajoute la recette qui est correspondant à l'id dans la route
         // dans les favoris.
@@ -51,7 +49,7 @@ class AccountController extends AbstractController
         $manager->persist($user);
         $manager->flush();
 
-        return $this->json("La recette a bien été ajoutée aux favoris.");
+        return $this->json(["color" => "green", "message" => "La recette a bien été ajoutée aux favoris.", "changeClass" => true]);
     }
 
     /**
@@ -63,13 +61,11 @@ class AccountController extends AbstractController
         $user = $this->getUser();
 
         // Si la recette n'est pas trouvée, on redirige vers la 404.
-        if (!$recipe) {
-            throw $this->createNotFoundException('Cette recette n\'existe pas');
-        }
+        if (!$recipe) return $this->json(["color" => "red", "message" => "Cette recette n'existe pas.", "changeClass" => false]);
 
         // Si l'utilisateur n'est pas connecté, on le renvoie sur
         // la page de connexion.
-        if (!$user) return $this->redirectToRoute('app_login');
+        if (!$user) return $this->json(["color" => "red", "message" => "Impossible, vous devez avoir un compte pour ajouter une recette à vos favoris.", "changeClass" => false]);
 
         // Ajoute la recette qui est correspondant à l'id dans la route
         // dans les favoris.
@@ -79,7 +75,7 @@ class AccountController extends AbstractController
         $manager->persist($user);
         $manager->flush();
 
-        return $this->json("La recette a bien été supprimée des favoris.");   
+        return $this->json(["color" => "red", "message" => "La recette a bien été supprimée des favoris.", "changeClass" => true]);   
     }
 
 }
