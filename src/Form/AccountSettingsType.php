5feb2d1e27af0f0f2a9dcfa04fee3b06b615aa35
D.K <70153500+DKHexDev@users.noreply.github.com>
2021-10-05 14:25:14 +0200

    Add Account Settings

---
+++ src/Form/AccountSettingsType.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
+class AccountSettingsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            //->add('email')
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'attr' => [
+                    'placeholder' => 'Pseudonyme'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Pseudonyme ne doit pas être vide !',
+                    ]),
+                ]
+            ])
+            ->add('oldPassword', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'attr' => [
+                    'placeholder' => 'Mot de passe actuel'
+                ],
+                'constraints' => [
+                    new UserPassword([
+                        'message' => 'Mauvais mot de passe',
+                    ]),
+                    new NotBlank([
+                        'message' => 'Veuillez entrer votre mot de passe actuel',
+                    ]),
+                ],
+                'mapped' => false
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Nouveau mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmer le nouveau mot de passe'
+                    ]
+                ],
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => [
+                    'autocomplete' => 'new-password'
+                ],
+                'constraints' => [
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit être au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
